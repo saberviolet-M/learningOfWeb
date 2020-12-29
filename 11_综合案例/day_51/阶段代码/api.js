6babafb74b829faabce7b9d5e9d35109d205bb48
Mat <1092203109@qq.com>
2020-12-29 17:32:46 +0800

    index页面素材导入

--- 11_综合案例/day_51/阶段代码/api.js
+++ 11_综合案例/day_51/阶段代码/api.js
@@ -3,6 +3,17 @@
 // 配置baseURL = 基地址
 axios.defaults.baseURL = 'http://ajax.frontend.itheima.net/';
 
+// 添加请求拦截器
+axios.interceptors.request.use(function (config) {
+    console.log("请求拦截器", config);
+    return config;
+}, function (error) {
+    // 对请求错误做些什么
+    return Promise.reject(error);
+});
+
+
+
 // 添加响应拦截器
 axios.interceptors.response.use(function (response) {
     console.log("响应拦截", response);
