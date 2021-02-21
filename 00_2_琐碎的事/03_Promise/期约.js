32701e76d86d4d3552779a445cf253affc79c348
Mat <1092203109@qq.com>
2021-02-21 14:38:36 +0800

    PromiseReviews

--- 00_2_琐碎的事/03_Promise/期约.js
+++
@@ -1,6 +0,0 @@
-// + ECMAScript 6 新增的引用类型 Promise，可以通过 new 操作符来实例化。
-let p = new Promise(() => {})
-setTimeout(console.log, 0, p) // Promise <pending>
-
-// * 期约状态机
-// + 期约的状态代表期约是否完成。 “待定 pending”表示尚未开始或者正在执行中。“兑现 fulfilled”表示已经成功完成，而“拒绝 rejected”则表示没有成功完成。
