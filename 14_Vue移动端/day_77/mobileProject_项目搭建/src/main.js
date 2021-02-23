d86c14de4f0cd754dbb74a88a06483d6f4eb36d3
Mat <1092203109@qq.com>
2021-02-23 14:27:07 +0800

    移动端项目搭建

---
+++ 14_Vue移动端/day_77/mobileProject_项目搭建/src/main.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import 'amfe-flexible'
+import http from './utils/http'
+
+Vue.config.productionTip = false
+Vue.prototype.$http = http
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
